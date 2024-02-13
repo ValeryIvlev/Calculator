4087d7b481e6dcfcd82ee71795787ce29974e6ef
Valery <qaivlev@yandex.ru>
2024-02-13 20:00:52 +0300

    12

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Scanner sc = new Scanner(System.in);
+        System.out.println("Калькулятор работает только с целыми числами");
         System.out.println("Введите математическое выражение из двух числ, например: 2 + 2 ");
         System.out.println("Поддерживаемые операции: +, -, *, /, ^, %");
         String mathFrom = sc.nextLine().strip();
